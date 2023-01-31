c5b9f88492acfc8a7e0cda2b3c1a02143cdf111e
devinfopercept <98084787+devinfopercept@users.noreply.github.com>
2023-01-31 12:41:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,15 @@ variable "virtual_network_id" {
   description = "The name of the resource group where the Azure DNS resides"
   type        = string
 }
+
+variable "private_dns_records_names" {
+  description = "The region where the keyvolt is created."
+  type = list(string)
+}
+
+variable "use_for_each" {
+  description = "Use `for_each` instead of `count` to create multiple resource instances."
+  type        = bool
+  default     = false
+  nullable    = false
+}
