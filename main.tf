72efbdd4b060c81cdb46565e244a44edc5962df6
devinfopercept <98084787+devinfopercept@users.noreply.github.com>
2023-01-31 12:40:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_zone_virtu
     azurerm_private_dns_zone.private_dns_zone
   ]
 }
+resource "azurerm_private_dns_a_record" "example" {
+  count1               = length(var.private_dns_records_names)
+  name                = (var.private_dns_records_names)[count1.index]
+  count               = length(var.private_dns_zone_names)
+  zone_name           = (var.private_dns_zone_names)[count.index]
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+  records             = ["10.230.112.20"]
+}
