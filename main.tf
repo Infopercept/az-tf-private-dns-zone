688c45fdd69d3819afc6d7feeb0c9b11f73e8335
Yash Bhatt <yash.b@kemsys.com>
2023-03-23 16:32:17 +0530

    Added new changes

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "azurerm_private_dns_a_record" "prdremarapp01" {
     azurerm_private_dns_zone.private_dns_zone
   ]
 }
+/*
 resource "azurerm_private_dns_a_record" "stgapigw-vip" {
   name                = "stgapigw-vip"
   zone_name           = "dm.ae"
@@ -35,8 +36,24 @@ resource "azurerm_private_dns_a_record" "stgapigw-vip" {
   depends_on = [
     azurerm_private_dns_zone.private_dns_zone
   ]
+}*/
+
+resource "azurerm_private_dns_a_record" "stgapigw-vip" {
+    fqdn                = "prdremarapp01.dm.ae."
+    id                  = "/subscriptions/9c6305ab-4484-4e63-9189-3ec249d383ae/resourceGroups/dm-it-iot-dev-connectivity-rg/providers/Microsoft.Network/privateDnsZones/dm.ae/A/prdremarapp01"
+    name                = "prdremarapp01"
+    records             = [
+        "10.230.112.20",
+    ]
+    resource_group_name = "dm-it-iot-dev-connectivity-rg"
+    tags                = {}
+    ttl                 = 3600
+    zone_name           = "dm.ae"
+
+    timeouts {}
 }
-resource "azurerm_private_dns_a_record" "tstremarapp01" {
+
+/*resource "azurerm_private_dns_a_record" "tstremarapp01" {
   name                = "tstremarapp01"
   zone_name           = "dm.ae"
   resource_group_name = var.resource_group_name
@@ -45,6 +62,21 @@ resource "azurerm_private_dns_a_record" "tstremarapp01" {
   depends_on = [
     azurerm_private_dns_zone.private_dns_zone
   ]
+}*/
+
+resource "azurerm_private_dns_a_record" "tstremarapp01" {
+    fqdn                = "stgapigw-vip.dm.ae."
+    id                  = "/subscriptions/9c6305ab-4484-4e63-9189-3ec249d383ae/resourceGroups/dm-it-iot-dev-connectivity-rg/providers/Microsoft.Network/privateDnsZones/dm.ae/A/stgapigw-vip"
+    name                = "stgapigw-vip"
+    records             = [
+        "10.140.70.174",
+    ]
+    resource_group_name = "dm-it-iot-dev-connectivity-rg"
+    tags                = {}
+    ttl                 = 36000
+    zone_name           = "dm.ae"
+
+    timeouts {}
 }
 
 
